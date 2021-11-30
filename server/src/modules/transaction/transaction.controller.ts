4e65e95fe9868cdaa97d0b7623558c35056c0292
thekareemone <thekareem13@gmail.com>
2021-11-30 19:06:08 +0200

    Fixed Server issues

--- server/src/modules/transaction/transaction.controller.ts
+++ server/src/modules/transaction/transaction.controller.ts
@@ -1,20 +1,16 @@
-import { Controller } from '@nestjs/common';
-import { TransactionService } from './transaction.service';
-import { UseGuards,Get,Post,Param,Body } from '@nestjs/common';
-import { TransactionDto } from './dto/transaction.dto';
+import { Controller } from "@nestjs/common";
+import { TransactionService } from "./transaction.service";
+import { UseGuards, Get, Post, Param, Body } from "@nestjs/common";
+import { TransactionDto } from "./dto/transaction.dto";
 
-
-@Controller('transactions')
+@Controller("transactions")
 export class TransactionController {
   constructor(private transactionService: TransactionService) {}
 
+  // @UseGuards()
+  // @Get(':accountID')
+  // transaction(@Param()accountID:number):any{
+  //   return this.transactionService.findTransaction(accountID);
 
-  @UseGuards()
-  @Get(':accountID')
-  transaction(@Param()accountID:number):any{
-    return this.transactionService.findTransaction(accountID);
-
-  }
-
-
+  // }
 }

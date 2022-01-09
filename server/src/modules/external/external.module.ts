671e7edf03ff07568dea6b6f94ed74bdac4f69f9
SohilaArafa <sohilaarafa2001@gmail.com>
2022-01-09 21:42:05 +0200

    module update

--- server/src/modules/external/external.module.ts
+++ server/src/modules/external/external.module.ts
@@ -3,19 +3,27 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ExternalController } from './external.controller';
 import { ExternalService } from './external.service';
 import { Transactions, TransactionSchema } from 'src/schemas/transaction.schema';
+import { TransactionModule } from '../transaction/transaction.module';
 import { AccountModule } from '../account/account.module';
-import { Account, AccountSchema } from 'src/schemas/account.schema';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 
 @Module({
   imports:[ 
-    MongooseModule.forFeature([{ name: Transactions.name, schema: TransactionSchema }]),
-    MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }]),
-    forwardRef(() => AccountModule)
+    
+          TransactionModule,
+          PassportModule,
+          JwtModule.register({
+          secret:"My-Secret-Key",
+          signOptions: { expiresIn: '5min' },
+          }),AccountModule
   ],
+     
+   
   controllers: [ExternalController],
   providers: [ExternalService],
-  exports: [ExternalService],
+  exports: [ExternalService,JwtService],
 })
 
-export class TransactionModule {}
+export class ExternalTransactionModule {}

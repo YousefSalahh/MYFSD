0259dcdcf3567de14797dcf341f82da94669888a
SohilaArafa <sohilaarafa2001@gmail.com>
2022-01-09 20:52:59 +0200

    secret

--- server/src/modules/external/strategy/Jwtstrategy.ts
+++ server/src/modules/external/strategy/Jwtstrategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreException: true,
-      secretOrKey:"sKey",
+      secretOrKey:"My_Secret_Key",
     });
   }
   /**

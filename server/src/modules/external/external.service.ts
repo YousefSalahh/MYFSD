2ea926499dabfaf5617024a5c7ef3148e7550154
SohilaArafa <sohilaarafa2001@gmail.com>
2022-01-10 14:38:58 +0200

    fixing balance check

--- server/src/modules/external/external.service.ts
+++ server/src/modules/external/external.service.ts
@@ -5,8 +5,6 @@ import axios from 'axios';
 import { externalDto } from "./dto/external.dto";
 import { AccountService } from "../account/account.service";
 import { JwtStrategy } from "./strategy/Jwtstrategy";
-import { JwtService } from "@nestjs/jwt";
-import { Response as res, Request as req} from "express";
 
 @Injectable()
 export class  ExternalService {
@@ -20,20 +18,19 @@ export class  ExternalService {
 //sending extrnal transaction
 async createExternalTransaction(request: any) {
    const balance = await this.accountService.getBalance((request).amount);
-    if(balance > request.amount+5 && request.amount <= 50) {        
+   /**
+    * get the balance of the user sending money
+    * check whether their enough funds to send amount + 5$
+    */
+    if(balance >= request.amount+5 && request.amount <= 50) {        
         let req: externalDto = {receiverAccNumber:request.receiverAccNumber,amount:request.amount,description:request.description};
         const token = await this.JwtStrategy.GenerateToken(req,Response);
         axios.post(`http://${request.url}/external/SendingExternalTransaction`, request,{headers:{'Authorization':`${token}`,'Bypass-Tunnel-Reminder':"any"}})
         .then(async(response)=>{
             if(response){ 
-            const insertTransaction : TransactionDto = {
-            dateOfToday : new Date() ,
-            amount : request.amount,
-            accountID : request.receiverAccNumber,
-            type : "debit",
-            transactionName : "External" ,
-            description : request.description
-            }
+            const insertTransaction : TransactionDto = {dateOfToday : new Date() ,amount : request.amount,accountID : request.receiverAccNumber,
+            type : "debit",transactionName : "External" ,description : request.description}
+            
             //inserting a recieved external transaction
             await this.transactionService.createTransaction(insertTransaction)
             //handling 5 dollar fee by posting another transaction
@@ -45,8 +42,7 @@ async createExternalTransaction(request: any) {
             this.accountService.updateSenderBalance(request.receiverAccNumber, 5);
             }
            
-        }
-        ) 
+        }) 
     }
         else {
             throw new HttpException('InSuffiecient Funds', HttpStatus.BAD_REQUEST);
